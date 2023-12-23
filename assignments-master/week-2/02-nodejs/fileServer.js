b5332ca6612351a005653944c1fe05a53875aadf
Rahulshinde01 <rahulshinde1063@gmail.com>
2023-12-24 02:12:50 +0530

    update solutions 2

--- assignments-master/week-2/02-nodejs/fileServer.js
+++ assignments-master/week-2/02-nodejs/fileServer.js
@@ -13,101 +13,49 @@
     Testing the server - run `npm run test-fileServer` command in terminal
  */
 
-// const express = require('express');
-// const fs = require('fs');
-// const path = require('path');
-// const app = express();
+    //main solution
+    // const express = require('express');
+    // const fs = require('fs');
+    // const path = require('path');
+    // const app = express();
+    
+    // app.get('/files', function (req, res) {
+    //     fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+    //     if (err) {
+    //         return res.status(500).json({ error: 'Failed to retrieve files' });
+    //     }
+        
+    //     res.json(files);
+    //     });
+    // });
+    
+    // app.get('/file/:filename', function (req, res) {
+    //     const filepath = path.join(__dirname, './files/', req.params.filename);
+    
+    //     fs.readFile(filepath, 'utf8', (err, data) => {
+    //     if (err) {
+    //         return res.status(404).send('File not found');
+    //     }
+    //     res.send(data);
+    //     });
+    // });
+    
+    // app.all('*', (req, res) => {
+    //     res.status(404).send('Route not found');
+    // });
+    
+    // module.exports = app;
 
-// app.get("/files/", function (req,res){
-//   fs.readdir("./files/", function (err,files){
 
-//     if (err) {
-//       return res.status(500).send("Internal Server Error");
-//     }
 
-//       res.status(200).json({
-//           files
-//       })
-//   })
-// })
-
-// app.get("/files/:filename", function (req,res){
-//   const name = req.params.filename;
-//   const myfile = path.join("./files",name);
-
-//   fs.readFile(myfile, "utf-8", function (err, data){
-//     if(err){
-//       res.status(404).send("File not found")
-//     }
-
-//     res.status(200).json({
-//       data
-//     })
-//   })
-// })
-
-// app.use(function (req,res){
-//   res.status(404).send("not found");
-// })
-
-// // app.listen(3000);
-
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
-
-// module.exports = app;
 
 
 
 
 //second solution
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const app = express();
-
-app.get("/files", function (req, res) {
-  const filesPath = "./files/";
-
-  fs.readdir(filesPath, function (err, files) {
-    if (err) {
-      return res.status(500).send("Internal Server Error");
-    }
-
-    res.status(200).json(files);
-  });
-});
-
-app.get("/file/:filename", function (req, res) {
-  const filename = req.params.filename;
-  const filePath = path.join("./files", filename);
-
-  fs.readFile(filePath, "utf-8", function (err, data) {
-    if (err) {
-      return res.status(404).send("File not found");
-    }
-
-    res.status(200).send(data);
-  });
-});
-
-// For any other route not defined, return 404
-app.use(function (req, res) {
-  res.status(404).send("Route not found");
-});
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
-
-module.exports = app;
-
-
-
-//third similar solution
-// const express = require('express');
-// const fs = require('fs');
-// const path = require('path');
+// const express = require("express");
+// const fs = require("fs");
+// const path = require("path");
 // const app = express();
 
 // app.get("/files", function (req, res) {
@@ -135,13 +83,51 @@ module.exports = app;
 //   });
 // });
 
-// // Handle invalid routes
+// // For any other route not defined, return 404
 // app.use(function (req, res) {
 //   res.status(404).send("Route not found");
 // });
 
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
 
 // module.exports = app;
+
+
+
+//third similar solution
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.get("/files", function (req, res) {
+  const filesPath = "./files/";
+
+  fs.readdir(filesPath, function (err, files) {
+    if (err) {
+      return res.status(500).send("Internal Server Error");
+    }
+
+    res.status(200).json(files);
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  const filename = req.params.filename;
+  const filePath = path.join("./files", filename);
+
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+
+    res.status(200).send(data);
+  });
+});
+
+// Handle invalid routes
+app.use(function (req, res) {
+  res.status(404).send("Route not found");
+});
+
+
+module.exports = app;
